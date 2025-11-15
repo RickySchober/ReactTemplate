01217f466d8ef5c6453defb73472412bf15983a1
Ricky Schober <rschober@scu.edu>
2025-11-14 17:39:02 -0800

    updated api url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000";
 
 const api = axios.create({
   baseURL: API_BASE,
+  withCredentials: false, // use true only if you use cookies
 });
 
 api.interceptors.request.use((config) => {

3fd5770a74be463dd36abf154db68e2c9b04783a
RickySchober <rschober@scu.edu>
2026-02-09 15:42:07 -0800

    small fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Routes } from "react-router-dom";
-import Button from "@/components/Button.js";
 
+import Button from "@/components/Button.js";
 
 export default function App() {
   return (
     <>
-        <Routes>
-        </Routes>
-        <Button>fix</Button>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">fix</button>
+      <Routes></Routes>
+      <Button>fix</Button>
+      <button className="rounded bg-blue-500 px-4 py-2 text-white">fix</button>
     </>
   );
 }
